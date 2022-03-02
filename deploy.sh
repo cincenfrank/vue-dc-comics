28f08908970284aae673e052c50bfcb7cb9f4fd0
cincenfrank <fpetrangeli1@gmail.com>
2022-03-02 16:34:28 +0100

    added deployment fixes

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:cincenfrank/vue-dc-comics.git master:gh-pages
+git push -f git@github.com:cincenfrank/vue-dc-comics.git main:gh-pages
 cd -
